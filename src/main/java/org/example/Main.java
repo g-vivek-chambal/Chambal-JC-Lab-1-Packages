4a07866a56e1b82def6b7d627a593579154f04d3
g-vivek-chambal <vivek-j.chambal@gcash.com>
2026-07-14 06:02:24 +0800

    Finish lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
-
 // Import here manually
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import packageone.ClassOne;
+import packagetwo.ClassTwo;
+import packagethree.ClassThree;
+
+
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println(new ClassOne().greet());
+        System.out.println(new ClassTwo().greet());
+        System.out.println(new ClassThree().greet());
     }
 }
